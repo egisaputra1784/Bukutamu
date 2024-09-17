022bbac53eef0e78ff5ce86c4ad4d8c80b9bcbbf
egisaputra1784 <eginovianisaputra313@gmail.com>
2024-09-17 09:27:17 +0700

    templating app buku tamu dengan sb admin 2

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include_once('template/header.php');
+?>
+
+<!-- Begin Page Content -->
+ <div class="container-fluid">
+
+<!-- Page Heading -->
+<h1 class="h3 mb-4 text-gray-800">Perong</h1>
+
+</div>
+<!-- /.container-fluid -->
